030336e55cdc651129d28609497fa317d26b5be0
#571 <31502293+number571@users.noreply.github.com>
2023-02-17 15:19:30 +0700

    Create primitive_number.py

---
+++ primitive_number.py
@@ -0,0 +1,17 @@
+# p = 2q+1, q = 5 - простое число
+# a^[(p-1)/2] != 1 && a^[(p-1)/5] != 1 => a - первообразный корень
+
+p = 11 # p - надёжное простое число
+
+print(f"(a^i) mod (p={p})\n")
+
+print(end="\t")
+for i in range(1, p):
+    print(f"i={i}", end="\t")
+print()
+
+for a in range(1, p):
+    print(f"a={a}", end="\t")
+    for i in range(1, p):
+        print((a**i)%p, end="\t")
+    print()
